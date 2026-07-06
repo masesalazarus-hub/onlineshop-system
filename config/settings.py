8a7caacf5b26863aa0f9539de7bb52300b8861a5
masesalazarus-hub <masesalazarus@gmail.com>
2026-07-06 09:30:35 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 LOGIN_URL = '/login/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
